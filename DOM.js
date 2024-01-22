78d16a3f1b151d463b58bb1970728b5bd88e5a4a
Arthur M <arthurmatias2002@hotmail.com>
2024-01-22 00:31:43 -0300

    dom_study_map_2

--- DOM.js
+++ DOM.js
@@ -2,25 +2,23 @@
 
 //ATRIBUTOS
 
-let element = document.querySelector('h3');
+let element = document.querySelector('h3')
 
 //hasAttribute
 if(element.hasAttribute('id')){ //hasAtribute pergunta se no elemente existe determinado atributo
     
-    console.log('SIM'); //aqui se o element tiver id vai printar "SIM" no console
+    console.log('SIM') //aqui se o element tiver id vai printar "SIM" no console
 }
 
 //getAttribute
-console.log(element.getAttribute('id')); //print no console o nome do id
+console.log(element.getAttribute('id')) //print no console o nome do id
 
 //setAttribute
-
-console.log(element.setAttribute('title','nome do titulo')); //cria-se um pop-up informando oque é cada elemento 
+console.log(element.setAttribute('title','nome do titulo')) //cria-se um pop-up informando oque é cada elemento 
 
 //removeAttribute
-
-let image = document.querySelector('img');
-image.removeAttribute('src'); //remove oque foi selecionado
+let image = document.querySelector('img')
+image.removeAttribute('src') //remove oque foi selecionado
 
 /*-------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -32,34 +30,48 @@ if(true){ //criar e apagar elemento
 
     function execute(){ //chamada no onclick do botão no html para ser executada 
 
-        if(k){
+        if(k == true){
 
             //CRIAR ELEMENTOS
-            new_element = document.createElement('p');
+            new_element = document.createElement('p')
     
             new_element.className = "alerta"; //nome do elemento
-            new_element.innerText = "oque eu quero que apareça"; //oque vai aparecer no elemento no caso um p identificado com innertext
+            new_element.innerText = "oque eu quero que apareça" //oque vai aparecer no elemento no caso um p identificado com innertext
     
             //ADD O ELEMENTO CRIADO
-            element_created = document.querySelector('.box');
-            element_created.appendChild(new_element);
+            element_created = document.querySelector('.box')
+            element_created.appendChild(new_element)
 
             k = false;
 
             console.log(k)
         }else{
 
-            element_created.removeChild(new_element); // remove o elemento
+            element_created.removeChild(new_element) // remove o elemento
 
             k = true;
         }
-
-    
     }
+/*-------------------------------------------------------------------------------------------------------------------------------*/
 
+//MÉTODOS PARA ADICIONAR ELEMENTOS
+
+//before para adicionar texto ou elementos antes da div selecionada
+//prepend para adicionar texto ou elementos no inicio da div selelcionada
+//append para adiconar texto ou elementos no final da div selecionada
+//after para adicionar texto ou elementos depois da div selecionada
+
+//CLONE
+let div = document.querySelector('#container').cloneNode(true) //clona a div selecionada, mas somente com true clona com os os elementos html
+document.querySelector('#container').after(div)
+
+//REMOVER ELEMENTOS
+document.querySelector('#container').remove() //remove oque foi selecionados
+
+//ALTERAR ESTILOS COM STYLE
+let p = document.querySelector('p')
+p.style.borderWidth = '2px' //variavavel.style.propriedade = modificação
+p.style.cssText = 'background-color: blue; color: white' //variavavel.style.propriedade = modificação
+
+/*-------------------------------------------------------------------------------------------------------------------------------*/
 }
-
-
-
-
-
